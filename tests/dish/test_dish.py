bff371058a821dc9410e91d57c715f4b5b679659
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 19:49:34 -0300

    testando se Dish foi instanciada corretamente

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,7 +1,12 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
-# import pytest
+from src.models.ingredient import Ingredient
+import pytest
 
 
 def test_dish():
     pass
-    # dish = Dish()
+    dish = Dish("lasanha berinjela", 27.00)
+    
+    assert dish.name == "lasanha berinjela"
+    assert dish.price == 27.00
+    assert dish.recipe == {}
