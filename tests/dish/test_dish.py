0fda4681c86ca17b1344353bdd0cdc97f5b5963c
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 17:54:08 -0300

    testando se a classe foi instanciada

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,7 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
-
+import pytest
 
 # Req 2
 def test_dish():
     pass
+    dish = Dish()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,12 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+import pytest
 
 
-# Req 1
 def test_ingredient():
     pass
+    ingrediente = Ingredient("queijo parmesão")
+    assert ingrediente.name == "queijo parmesão"
+
+    ingredient = Ingredient("bacon")
+    assert ingredient.name == "bacon"
+
