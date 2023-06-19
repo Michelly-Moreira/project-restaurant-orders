1c41769c47b0c41cf852cab66c8ebcbc83ea4b73
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 19:03:21 -0300

    concluindo o req1

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,7 +1,7 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
-import pytest
+# import pytest
 
-# Req 2
+
 def test_dish():
     pass
-    dish = Dish()
+    # dish = Dish()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,22 +1,27 @@
 from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
-import pytest
 
 
 def test_ingredient():
-    pass
+
     ingrediente = Ingredient("queijo parmesão")
     assert ingrediente.name == "queijo parmesão"
     ingredient = Ingredient("bacon")
     assert ingredient.name == "bacon"
 
-    assert ingrediente.restrictions == {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
-    assert ingredient.restrictions == {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+    assert ingrediente.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+        }
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED
+        }
 
     assert ingrediente.__repr__(ingrediente) == "Ingredient('queijo parmesão')"
     assert ingrediente.__repr__(ingredient) == "Ingredient('bacon')"
 
-    assert ingredient.__eq__(ingredient) == True
-    assert ingrediente.__eq__(ingredient) == False
+    assert ingredient.__eq__(ingredient) is True
+    assert ingrediente.__eq__(ingredient) is False
 
     assert ingrediente.__hash__() == ingrediente.__hash__()
     assert ingrediente.__hash__() != ingredient.__hash__()
