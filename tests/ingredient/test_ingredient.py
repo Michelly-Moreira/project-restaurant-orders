a9d3cddf08ef72c67ff2841da776689f0d627f71
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 18:14:10 -0300

    testando restrições alimentares e método mágico repr

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,4 +1,4 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 import pytest
 
 
@@ -6,7 +6,11 @@ def test_ingredient():
     pass
     ingrediente = Ingredient("queijo parmesão")
     assert ingrediente.name == "queijo parmesão"
-
     ingredient = Ingredient("bacon")
     assert ingredient.name == "bacon"
 
+    assert ingrediente.restrictions == {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.restrictions == {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+
+    assert ingrediente.__repr__ == "Ingredient('queijo parmesão')"
+    assert ingredient.__repr__ == "Ingredient('bacon')"
