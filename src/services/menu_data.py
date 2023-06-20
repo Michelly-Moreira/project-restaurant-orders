b09df4e026960d353cbc98f560c20a0c5cb1f9ee
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-20 17:26:45 -0300

    leitura do arquivo csv

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,20 @@
-# Req 3
+from models.dish import Dish
+from models.ingredient import Ingredient
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.source_path = source_path
+        self.dish = set()
+        self.ingredient = set()
+
+    def reading_file(source_path):
+        with open(source_path,  encoding = "utf-8") as file:
+            source = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = source
+            print(header)
+            print(data)
+# Quando um * está presente no desempacotamento, os valores são desempacotados em formato de lista.
+
+MenuData.reading_file("/home/michelly/Trybe/trybe-project/cs/sd-026-b-restaurant-orders/data/menu_base_data.csv")
